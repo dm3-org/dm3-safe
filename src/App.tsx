715249bbef3221aaf99cf9ad3ac403966a21709e
AlexNi245 <alex.plutta@googlemail.com>
2023-08-23 11:27:20 +0200

    use express server to serve static web files

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,50 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk';
-import { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { useSafeAppsSDK } from "@safe-global/safe-apps-react-sdk";
+import { useEffect, useState } from "react";
 
 //@ts-ignore
-import { DM3 } from 'dm3-react';
+import { DM3 } from "dm3-react";
 
 function App() {
-    const { sdk, safe } = useSafeAppsSDK();
-    sdk.safe.getEnvironmentInfo();
+  const { sdk, safe } = useSafeAppsSDK();
+  sdk.safe.getEnvironmentInfo();
 
-    useEffect(() => {
-        safe.owners.forEach((owner) => {
-            const ensName = `${owner}${process.env.REACT_APP_ADDR_ENS_SUBDOMAIN}`;
-            setOwnersEnsNames((ownersEnsNames) => [...ownersEnsNames, ensName]);
-        });
-    }, []);
+  useEffect(() => {
+    safe.owners.forEach((owner) => {
+      const ensName = `${owner}${process.env.REACT_APP_ADDR_ENS_SUBDOMAIN}`;
+      setOwnersEnsNames((ownersEnsNames) => [...ownersEnsNames, ensName]);
+    });
+  }, []);
 
-    const [ownersEnsNames, setOwnersEnsNames] = useState<string[]>([]);
+  const [ownersEnsNames, setOwnersEnsNames] = useState<string[]>([]);
 
-    return (
-        <>
-            <DM3
-                defaultContacts={['help.dm3.eth', ...ownersEnsNames]}
-                defaultServiceUrl={process.env.REACT_APP_DEFAULT_SERVICE}
-                showAlways={true}
-                // connectionStateChange={(state: ConnectionState) =>
-                //     setShowLogo(state === ConnectionState.SignedIn)
-                // }
-            />
-            <div className="logo"></div>
-            <nav
-                className="navbar fixed-bottom navbar-light "
-                style={{ backgroundColor: '#000000ff !important' }}
-            >
-                <div className="container-fluid text-center ">
-                    <div className="w-100">
-                        <a
-                            className="text-muted legal"
-                            href="https://dm3.network/privacy-policy/"
-                        >
-                            Privacy Policy
-                        </a>
-                        <a
-                            className="text-muted legal ms-4"
-                            href="https://dm3.network/terms-and-conditions/"
-                        >
-                            Terms & Conditions
-                        </a>
-                    </div>
-                </div>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <DM3
+        defaultContacts={["stkux.eth", "help.dm3.eth", ...ownersEnsNames]}
+        defaultServiceUrl={process.env.REACT_APP_DEFAULT_SERVICE}
+        showAlways={true}
+        // connectionStateChange={(state: ConnectionState) =>
+        //     setShowLogo(state === ConnectionState.SignedIn)
+        // }
+      />
+      <div className="logo"></div>
+      <nav className="navbar fixed-bottom navbar-light " style={{ backgroundColor: "#000000ff !important" }}>
+        <div className="container-fluid text-center ">
+          <div className="w-100">
+            <a className="text-muted legal" href="https://dm3.network/privacy-policy/">
+              Privacy Policy
+            </a>
+            <a className="text-muted legal ms-4" href="https://dm3.network/terms-and-conditions/">
+              Terms & Conditions
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default App;
+//0x4d7668C28e5d020Ac1b4711AfAd3b72344fa36F6
---
+++ src/express.ts
@@ -0,0 +1,18 @@
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import path from 'path';
+
+
+const app = express();
+const server = http.createServer(app);
+
+//TODO remove
+app.use(cors());
+
+
+// TODO Get rif if DO deployment is ready
+app.use(express.static(path.join(__dirname, './build')));
+const port = process.env.PORT || '8080';
+
+server.listen(port, () => console.log("Safe app started at port " + port));
